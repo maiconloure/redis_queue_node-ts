12a75407524c9f8272b029ebf538e1102a27c61d
Maicon Lourenço <maiconloure@gmail.com>
2021-03-05 01:53:12 -0300

    initial commit

---
+++ src/app/jobs/RegistrationMail.js
@@ -0,0 +1,15 @@
+import Mail from '../lib/Mail';
+
+export default {
+  key: 'RegistrationMail',
+  async handle({ data }) {
+    const { user } = data;
+
+    await Mail.sendMail({
+      from: 'Redis Queue Test <redisqueue@test.com>',
+      to: `${user.name} <${user.email}>`,
+      subject: 'Cadastro de usuario',
+      html: `Olá, ${user.name}, bem-vindo ao sistema de filas da Rocketseat :D`
+    })
+  }
+}
